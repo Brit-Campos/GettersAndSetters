a3409c2d6e8806b1a348c49960d2a4b9cc979a51
Brittanie Campos <brittanie.angelique@gmail.com>
2022-06-27 14:56:20 -0700

    Add project files.

---
+++ GettersAndSetters/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // getters & setters = add security to fields by enscapulation
+        //                     They're accessors found within properties
+
+        // properties = combine aspects of both fields and methods (share name with a field)
+        // get accessor = used to return the property value
+        // get accessor = used to assign a new value
+        // value keyword = defines the value being assigned by the set (parameter)
+
+        Car car = new Car(400);
+
+        car.Speed = 1000000;
+
+        Console.WriteLine(car.Speed);
+
+        Console.ReadKey();
+    }
+}
+
+class Car
+{
+    private int speed;
+
+    public Car(int speed)
+    {
+        Speed = speed;
+    }
+
+    public int Speed
+    {
+        get { return speed; } //read
+        set 
+        { 
+            if(value > 500)
+            {
+                speed = 500;
+            }
+            else 
+            { 
+                speed = value; 
+            }
+            
+        } 
+    }
+}
